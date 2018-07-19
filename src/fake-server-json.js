f1007891ffce36b52ed63cab84093f6dd7fcea8d
Ilya <ilyarbox4@gmail.com>
2018-07-19 16:54:42 +0500

    Make fake-ready json for profile data

---
+++ src/fake-server-json.js
@@ -0,0 +1,11 @@
+var profileInfo = {
+  page: {
+    avatar: 'https://pp.userapi.com/c638218/v638218049/7d400/hcV5FfVV5SA.jpg?ava=1',
+    name: 'Ilya Blinkov',
+    currentInfo: 'Sometimes the same is different...'
+  }
+};
+
+export {
+  profileInfo
+};
