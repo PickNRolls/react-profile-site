47ea3bc34b05b1c26943dfc28580756a6624dde4
Ilya <ilyarbox4@gmail.com>
2018-07-22 00:18:59 +0500

    Edit html structure of page-info

--- src/layout/profile-page/info/index.js
+++ src/layout/profile-page/info/index.js
@@ -14,17 +14,19 @@ class PageInfo extends React.Component {
     var name = this.props.name
 
     return (
-      <div className="page-info page-block">
-        <div className="page-info__main">
-          <div className="page-info__online-status">
-            <span>Online</span>
+      <div className="page-info page-block page-block--wrap">
+        <div className="page-block page-block--inner">
+          <div className="page-info__main">
+            <div className="page-info__online-status">
+              <span>Online</span>
+            </div>
+            <h1 className="page-info__profile-name">
+              {name.first} {name.second}
+            </h1>
           </div>
-          <h1 className="page-info__profile-name">
-            {name.first} {name.second}
-          </h1>
-        </div>
-        <div className="page-info__profile-info">
-          
+          <div className="page-info__profile-info">
+            
+          </div>
         </div>
       </div>
     );
