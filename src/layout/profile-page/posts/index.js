75cdb507c761719db0c956b23bc777e2a0978106
Ilya <ilyarbox4@gmail.com>
2018-07-26 19:47:56 +0500

    Add condition to resolve no posts view

--- src/layout/profile-page/posts/index.js
+++ src/layout/profile-page/posts/index.js
@@ -5,7 +5,16 @@ import './main.css';
 class PagePosts extends React.Component {
   getPosts () {
     this.wall = this.props.wall;
-    this.posts = this.props.wall.posts.map(function (post) {
+    if (!this.wall.posts.length) {
+      return (
+        <div className="page-no-post page-block page-block--wrap">
+          <img src="https://vk.com/images/no_posts.png" alt="no posts" className="page-no-post__pic"/>
+          <span className="page-no-post__text">На стене еще нет записей.</span>
+        </div>
+      );
+    }
+
+    this.posts = this.wall.posts.map(function (post) {
       return (
         <div className="page-post page-block" key={post.id}>
           {post.content.text}
