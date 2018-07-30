b7d2205bf0883252f50a2e248985adb3ff4757bb
Ilya <ilyarbox4@gmail.com>
2018-07-30 18:29:20 +0500

    Start change database handlers from fake to true

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,40 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var config = require('./config');
 
+var db;
+var app = express();
+var router = express.Router();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 mongoose.connect(config.mongo.uri, config.mongo.options);
-var db = mongoose.connection;
+db = mongoose.connection;
+
+var User = require('./models/user');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+router.get('/users/:id', function (req, res) {
+  var userID = req.params.id;
+
+  User.findOne({_id: userID}, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  })
+});
+
+app.use(router);
+
+
+app.listen(8080, function () {
+  console.log('Express is at 8080');
+});
 
 module.exports = mongoose;
--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,4 +1,4 @@
-var mongoose = require('../index.js');
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
